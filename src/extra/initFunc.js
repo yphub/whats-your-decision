5ab126179a851739e41b6d6c5551ecd347c391df
535939238 <535939238@qq.com>
2018-06-19 15:28:16 +0800

    change version

--- src/extra/initFunc.js
+++ src/extra/initFunc.js
@@ -14,7 +14,7 @@ export default function () {
   }
 
   //版本检测
-  var __ver__ = "1.2.7";
+  var __ver__ = "1.2.9";
   var ver = wx.getStorageSync("appversion");
   if (ver !== __ver__) {
     wx.clearStorageSync();
